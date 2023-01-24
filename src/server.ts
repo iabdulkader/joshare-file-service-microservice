c35e072692cd029c2fb4f9432826de510d93e394
iabdulkader <abdulkadernow@gmail.com>
2023-01-24 20:18:41 +0600

    Minor

--- src/server.ts
+++ src/server.ts
@@ -50,25 +50,21 @@ app.get("/api/download/:id", downloadController);
 app.post("/api/delete", auth, deleteController);
 
 io.on("connection", (socket: Socket) => {
-    console.log(socket.id);
 
     socket.on("join", (data) => {
         socket.join(data.pin);
-        console.log("join", data.pin);
+        console.log("joined", data.pin);
     })
 
     socket.on("upload-progress", (data) => {
-        console.log("upload-progress", data);
         console.log(socket.to(data.pin).emit("upload-progress", data));
     })
 
     socket.on("upload-complete", (data) => {
-        console.log("upload-complete", data);
         socket.to(data.pin).emit("upload-complete", data);
     })
 
     socket.on("delete-file", (data) => {
-        console.log("delete-file", data);
         socket.to(data.pin).emit("delete-file", data);
     })
 
